45a2724f80fe6e9d3637a60e8ac187ed84872921
Camille Boulière <camillebouliere@gmail.com>
2016-05-17 20:45:17 +0200

    Init

---
+++ correct_my_french.php
@@ -0,0 +1,13 @@
+<?php
+
+// Correct my french
+function correct_my_french($text) {
+
+    $bad_french = array(' ?', ' !', ' :');
+    $good_french = array('&nbsp;?', '&nbsp;!', '&nbsp;:');
+
+    $text = str_replace($bad_french, $good_french, $text);
+
+    return $text;
+
+}
